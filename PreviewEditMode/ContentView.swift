2a19a6a3f0ea5f8d6041781840527d776a8606f0
Paul Wood <paul.wood.iii@gmail.com>
2019-07-07 10:27:23 -0400

    Initial Commit

---
+++ PreviewEditMode/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PreviewEditMode
+//
+//  Created by Paul Wood on 7/7/19.
+//  Copyright © 2019 Paul Wood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
